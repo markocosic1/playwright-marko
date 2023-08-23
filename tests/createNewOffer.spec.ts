dbbf049f8036eed0bba1546f3dce5d5ce63ffed2
markocosic1 <marko.cosic@cobeisfresh.com>
2023-08-23 11:32:28 +0200

    Create new offer test

--- tests/createNewOffer.spec.ts
+++ tests/createNewOffer.spec.ts
@@ -5,15 +5,13 @@ import { HomePage } from '../POMs/homePage';
 import { CreateNewOfferModal } from '../POMs/newOfferModal';
 import { CreateNewOfferEnvironments } from '../environments/createNewOfferEnvironments';
 
-test.beforeEach(async({ page }) => {
+test('Create new offer - client with no projects', async({ page }) => {
     const loginPage = new LoginPage(page);
     const loginEnvironments = new LoginEnvironments(page);
-
     await page.goto(loginEnvironments.baseUrl);
     await loginPage.login(loginEnvironments.validEmail, loginEnvironments.validPassword);
-});
+    await loginPage.assertLoginIsSuccessful();
 
-test('Create new offer - client with no projects', async({ page }) => {
     const homePage = new HomePage(page);
     await homePage.createNewOffer();
 
