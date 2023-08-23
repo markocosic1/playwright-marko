10e03531cc98d4a5663f20abdf3a4b3732e7543f
markocosic1 <marko.cosic@cobeisfresh.com>
2023-08-23 08:45:21 +0200

    Commit

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,10 +2,27 @@ import { test } from '@playwright/test';
 import { LoginPage } from '../POMs/loginPage';
 import { LoginEnvironments } from '../environments/loginEnvironments';
 
-test('Login with correct credentials', async ({ page }) => {
+test.beforeEach(async({ page }) => {
+    const loginPage = new LoginPage(page);
+    const loginEnvironments = new LoginEnvironments(page);
+
+    await loginPage.login(loginEnvironments.validEmail, loginEnvironments.validPassword);
+});
+
+test('Login - correct credentials', async({ page }) => {
     const loginPage = new LoginPage(page);
     const loginEnvironments = new LoginEnvironments(page);
 
     await page.goto(loginEnvironments.baseUrl);
     await loginPage.login(loginEnvironments.validEmail, loginEnvironments.validPassword);
+    await loginPage.assertLoginIsSuccessful();
+});
+
+test('Login - incorrect credentials', async({ page }) => {
+    const loginPage = new LoginPage(page);
+    const loginEnvironments = new LoginEnvironments(page);
+
+    await page.goto(loginEnvironments.baseUrl);
+    await loginPage.login(loginEnvironments.invalidEmail, loginEnvironments.invalidPassword);
+    await loginPage.assertErrorMessageIsVisible();
 });
